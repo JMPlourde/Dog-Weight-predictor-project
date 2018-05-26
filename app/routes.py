ecd5077dd8354b56bf57e5116c80a77fe38c4d70
Jessica Plourde <jessica.marie.plourde@gmail.com>
2018-05-26 11:13:50 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,26 @@
 from flask import render_template
 from app import app
 from app.forms import DogBreedForm
+import csv
 
 @app.route('/')
 @app.route('/index')
 def index(): #makes the dogbreedform the welcome page
     form = DogBreedForm()
     return render_template('breedform.html', title = 'Dog Info', form=form)
+#@app.route('/load')
+#def load(): #loads data
+    # def load_resources(self):
+    #   with open('dogweight.csv', 'rb') as dogweightcsv: #open csv
+     #      reader = csv.reader(dogweightcsv, delimiter=csv.excel.delimiter, quotechar=csv.excel.quotechar)    #load each row as an instance (dataitem) of dogbreedclass
+      #     next(reader, None)  # skip the headers
+       #    for row in reader:
+        #       data = Muttweight(id=row[0], breed=row[1], minweight=row[2], maxweight=row[3])
+         #      db.session.add(resource)
+          # print("Resources loaded.  There are now %i resources into the database." % db.session.query(ThrivResource).count())
+       #db.session.commit()
     
+#def getbreeds():
+#    for line in db:
+        
+        
