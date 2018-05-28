35137a15096be8e1b349834eae88cb34f82c7677
JMPlourde <jessica.marie.plourde@gmail.com>
2018-05-28 14:57:22 -0400

    Serializing data

--- app/models.py
+++ app/models.py
@@ -3,13 +3,21 @@ from app import db
 
 class DogBreeds(db.Model):
     __tablename__='DogBreeds'
-    id = db.Column(db.Integer, primary_key = True)
+    breedid = db.Column(db.Integer, primary_key = True)
     breedname = db.Column(db.String, index = True, unique = True)
     minweight = db.Column(db.Integer, index=True, unique = False)
     maxweight = db.Column(db.Integer, index = True, unique = False)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.breedid,'breedname': self.breedname,'minweight': self.minweight,'maxweight': self.maxweight}
+
 def __repr__(self):
     return '<DogBreeds {}>'.format(self.breedname)
+
+
 
 db.create_all()
 db.session.commit()
--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,31 @@
 from app import app, db
 import csv
 import flask
+from flask import json, jsonify
 
 from app.models import DogBreeds
 
 
-
-
 @app.route('/')
 @app.route('/index')
-def index(): 
-    message = { "hello":"world"}
+def index():
+    message = {"hello": "world"}
     return flask.jsonify(message)
+
 
 @app.route('/load')
 def load_resources():
-    with open('app/dogweight.csv', 'rt') as dogweightcsv: #open csv
-        reader = csv.reader(dogweightcsv, delimiter=",")    #load each row as an instance (dataitem) of dogbreedclass
+    with open('app/dogweight.csv', 'rt') as dogweightcsv:  # open csv
+        reader = csv.reader(dogweightcsv, delimiter=",")  # load each row as an instance (dataitem) of dogbreedclass
         print(reader)
         next(reader, None)  # skip the headers
         for row in reader:
-            data = DogBreeds(id=row[0], breedname=row[1], minweight=row[2], maxweight=row[3])
+            data = DogBreeds(breedid=row[0], breedname=row[1], minweight=row[2], maxweight=row[3])
             db.session.add(data)
         return "Resources loaded.  There are now %i resources into the database." % db.session.query(DogBreeds).count()
         db.session.commit()
-    
-#def getbreeds():
-#    for line in db:
-        
-        
+
+
+@app.route('/get')
+def getbreeds():
+    return jsonify(DogBreedWeight=[i.serialize for i in DogBreeds])
